5f16705b16d8d180de76bf85bd6809985a764cec
Yaroslav Senchin <93538385+KapikPro@users.noreply.github.com>
2025-02-12 00:41:29 +0300

    added Tests.Common

---
+++ ECC.DanceCup.Auth.Domain.Tests.Common/Attributes/InlineAutoMoqDataAttribute.cs
@@ -0,0 +1,13 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using AutoFixture.Xunit2;
+
+namespace ECC.DanceCup.Auth.Domain.Tests.Common.Attributes;
+
+public class InlineAutoMoqDataAttribute : InlineAutoDataAttribute
+{
+    public InlineAutoMoqDataAttribute(params object?[] objects)
+        : base(() => new Fixture().Customize(new AutoMoqCustomization()).Customize(new DomainCustomization()), objects)
+    {
+    }
+}
---
+++ ECC.DanceCup.Auth.Domain.Tests.Common/Customizations/DomainCustomization.cs
@@ -0,0 +1,22 @@
+﻿using AutoFixture;
+using ECC.DanceCup.Auth.Domain.Model;
+
+namespace ECC.DanceCup.Auth.Domain.Tests.Common;
+
+public class DomainCustomization : ICustomization
+{
+    public void Customize(IFixture fixture)
+    {
+        fixture.Customize<User>(composer =>
+            composer.FromFactory(() => new User(
+                id: fixture.Create<long>(),
+                version: fixture.Create<int>(),
+                createdAt: fixture.Create<DateTime>(),
+                changedAt: fixture.Create<DateTime>(),
+                username: fixture.Create<string>(),
+                passwordHash: fixture.Create<byte[]>(),
+                passwordSalt: fixture.Create<byte[]>()
+            ))
+        );
+    }
+}
