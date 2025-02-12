5a38439d53e0a75f321e1596afb756c049319af2
Yaroslav Senchin <93538385+KapikPro@users.noreply.github.com>
2025-02-12 20:43:31 +0300

    fix

--- ECC.DanceCup.Auth.Domain.Tests.Common/Attributes/AutoMoqDataAttribute.cs
+++ ECC.DanceCup.Auth.Domain.Tests.Common/Attributes/AutoMoqDataAttribute.cs
@@ -7,7 +7,7 @@ namespace ECC.DanceCup.Auth.Domain.Tests.Common.Attributes;
 public class AutoMoqDataAttribute : AutoDataAttribute
 {
     public AutoMoqDataAttribute()
-        : base(() => new Fixture().Customize(new AutoMoqCustomization()).Customize(new DomainCustomization()))
+        : base(() => new Fixture().Customize(new AutoMoqCustomization()))
     {
     }
 }
--- ECC.DanceCup.Auth.Domain.Tests.Common/Attributes/InlineAutoMoqDataAttribute.cs
+++ ECC.DanceCup.Auth.Domain.Tests.Common/Attributes/InlineAutoMoqDataAttribute.cs
@@ -7,7 +7,7 @@ namespace ECC.DanceCup.Auth.Domain.Tests.Common.Attributes;
 public class InlineAutoMoqDataAttribute : InlineAutoDataAttribute
 {
     public InlineAutoMoqDataAttribute(params object?[] objects)
-        : base(() => new Fixture().Customize(new AutoMoqCustomization()).Customize(new DomainCustomization()), objects)
+        : base(() => new Fixture().Customize(new AutoMoqCustomization()), objects)
     {
     }
 }
