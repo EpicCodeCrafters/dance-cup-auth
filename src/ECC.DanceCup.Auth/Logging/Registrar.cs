1de5c8deab4e178926944e59f86d4a30a6564994
ZISRF <zeleninivans@gmail.com>
2026-01-29 18:07:10 +0300

    logs

---
+++ src/ECC.DanceCup.Auth/Logging/Registrar.cs
@@ -0,0 +1,36 @@
+﻿using ECC.DanceCup.Auth.Utils;
+using Serilog;
+using Serilog.Sinks.Kafka;
+
+namespace ECC.DanceCup.Auth.Logging;
+
+public static class Registrar
+{
+    public static IServiceCollection AddCustomLogging(
+        this IServiceCollection services,
+        IConfiguration configuration,
+        IWebHostEnvironment environment)
+    {
+        services
+            .AddSerilog(loggerConfiguration => loggerConfiguration
+                .ReadFrom.Configuration(configuration)
+                .Enrich.FromLogContext()
+                .Enrich.WithProperty("Service", Constants.ServiceName)
+                .Enrich.WithProperty("Environment", environment.EnvironmentName)
+                .WriteTo.Kafka(
+                    bootstrapServers: configuration["KafkaOptions:BootstrapServers"],
+                    topic: configuration["KafkaOptions:Topics:DanceCupLogs:Name"]
+                )
+                .WriteTo.Console()
+            );
+        
+        return services;
+    }
+
+    public static IApplicationBuilder UseCustomLogging(this IApplicationBuilder applicationBuilder)
+    {
+        applicationBuilder.UseSerilogRequestLogging();
+        
+        return applicationBuilder;
+    }
+}
